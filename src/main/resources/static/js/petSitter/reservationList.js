8d5912a256bcf2592cd5dfb685be8d4dfd656788
spehil <boo1231@naver.com>
2023-10-05 17:03:56 +0900

    feat/#28/ calendar 이전달 다음달 이동, 사이드바

--- src/main/resources/static/js/petSitter/reservationList.js
+++ src/main/resources/static/js/petSitter/reservationList.js
@@ -50,12 +50,45 @@ function removeCalendar(){
 
 
 
+function showMain(){
+    mainTodayDay.innerHTML = dayList[today.getDay()];
+    mainTodayDate.innerHTML = today.getDate();
+}
+var clickedDate1 = document.getElementById(today.getDate());
+clickedDate1.classList.add('active');
+var prevBtn = document.getElementById('prev');
+var nextBtn = document.getElementById('next');
+prevBtn.addEventListener('click',prev);
+nextBtn.addEventListener('click',next);
+var tdGroup = [];
+function clickStart(){
+    for(let i = 1; i <= pageYear[first.getMonth()]; i++){
+        tdGroup[i] = document.getElementById(i);
+        tdGroup[i].addEventListener('click',changeToday);
+    }
+}
+function changeToday(e){
+    for(let i = 1; i <= pageYear[first.getMonth()]; i++){
+        if(tdGroup[i].classList.contains('active')){
+            tdGroup[i].classList.remove('active');
+        }
+    }
+    clickedDate1 = e.currentTarget;
+    clickedDate1.classList.add('active');
+    today = new Date(today.getFullYear(), today.getMonth(), clickedDate1.id);
+    showMain();
+    keyValue = today.getFullYear() + '' + today.getMonth()+ '' + today.getDate();
+    reshowingList();
+}
+
+
+
+
 
 
 
 
 function prev(){
-    inputBox.value = "";
     const $divs = document.querySelectorAll('#input-list > div');
     $divs.forEach(function(e){
         e.remove();
@@ -87,8 +120,8 @@ function prev(){
     reshowingList();
 }
 
+
 function next(){
-    inputBox.value = "";
     const $divs = document.querySelectorAll('#input-list > div');
     $divs.forEach(function(e){
         e.remove();
@@ -112,7 +145,7 @@ function next(){
     today = new Date(today.getFullYear(), today.getMonth() + 1, today.getDate());
     currentTitle.innerHTML = monthList[first.getMonth()] + '&nbsp;&nbsp;&nbsp;&nbsp;'+ first.getFullYear();
     removeCalendar();
-    showCalendar();
+
     showMain();
     clickedDate1 = document.getElementById(today.getDate());
     clickedDate1.classList.add('active');
@@ -120,46 +153,3 @@ function next(){
     reshowingList();
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-function showMain(){
-    mainTodayDay.innerHTML = dayList[today.getDay()];
-    mainTodayDate.innerHTML = today.getDate();
-}
-var clickedDate1 = document.getElementById(today.getDate());
-clickedDate1.classList.add('active');
-var prevBtn = document.getElementById('prev');
-var nextBtn = document.getElementById('next');
-prevBtn.addEventListener('click',prev);
-nextBtn.addEventListener('click',next);
-var tdGroup = [];
-function clickStart(){
-    for(let i = 1; i <= pageYear[first.getMonth()]; i++){
-        tdGroup[i] = document.getElementById(i);
-        tdGroup[i].addEventListener('click',changeToday);
-    }
-}
-function changeToday(e){
-    for(let i = 1; i <= pageYear[first.getMonth()]; i++){
-        if(tdGroup[i].classList.contains('active')){
-            tdGroup[i].classList.remove('active');
-        }
-    }
-    clickedDate1 = e.currentTarget;
-    clickedDate1.classList.add('active');
-    today = new Date(today.getFullYear(), today.getMonth(), clickedDate1.id);
-    showMain();
-    keyValue = today.getFullYear() + '' + today.getMonth()+ '' + today.getDate();
-    reshowingList();
-}
