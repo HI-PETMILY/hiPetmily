c245fdaa9bd8a7c9759c9f0e7800f986bcb26483
brorsoo <kimsu010@naver.com>
2023-10-12 15:54:27 +0900

    feature/#30/petSitterRegist 작업환경분리

---
+++ src/main/java/com/mypet/petmily/petSitter/dao/PetSitterMapper.java
@@ -0,0 +1,13 @@
+package com.mypet.petmily.petSitter.dao;
+
+import com.mypet.petmily.petSitter.dto.*;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface PetSitterMapper {
+
+
+
+}
---
+++ src/main/java/com/mypet/petmily/petSitter/service/PetSitterService.java
@@ -0,0 +1,18 @@
+package com.mypet.petmily.petSitter.service;
+
+import com.mypet.petmily.petSitter.dao.PetSitterMapper2;
+import com.mypet.petmily.petSitter.dto.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@Transactional
+public class PetSitterService {
+
+
+
+}
