2667d192f542052b87a275c7dd82f24c84292873
HI-SAYA <qnfnsk45@naver.com>
2023-10-13 09:25:01 +0900

    feat/#37/지난 예약 수정

--- src/main/java/com/mypet/petmily/payment/controller/PaymentController.java
+++ src/main/java/com/mypet/petmily/payment/controller/PaymentController.java
@@ -2,6 +2,7 @@ package com.mypet.petmily.payment.controller;
 
 
 import com.mypet.petmily.member.dto.MemberDTO;
+import com.mypet.petmily.payment.dto.ReservationHistoryDTO;
 import com.mypet.petmily.payment.service.PaymentService;
 import com.mypet.petmily.petSitterNew.dto.ReservationDTO;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +11,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -30,14 +32,15 @@ public class PaymentController {
 
     /* 지난 예약 조회로 이동 */
     @GetMapping("/reservationList")
-    public void reservationHistoryPage(@AuthenticationPrincipal MemberDTO loginMember, Model model) {
+    public void reservationHistoryPage(@AuthenticationPrincipal MemberDTO loginMember,
+                                       Model model, @ModelAttribute ReservationHistoryDTO selectReserve) {
 
+//        selectReserve.setResMember(loginMember);
 
-        model.addAttribute("loginMember", loginMember.getMemberId());
+        model.addAttribute("loginMember", loginMember.getMemberNo());
         log.info("loginMember : {}", loginMember);
         List<ReservationDTO> reservationHistoryList = paymentService.selectReservationHistoryList(loginMember);
         log.info("reservationHistoryList : {}", reservationHistoryList);
-
         model.addAttribute("reservationHistoryList", reservationHistoryList);
 
     }
