f42f76e7ca6e70c23a011709ac78e9bce3d716a5
jooseohyeon <wntjguswkd@gmail.com>
2023-10-14 09:39:53 +0900

    feat/#34/후기 관련

--- src/main/java/com/mypet/petmily/review/dto/ReviewDTO.java
+++ src/main/java/com/mypet/petmily/review/dto/ReviewDTO.java
@@ -1,6 +1,7 @@
 package com.mypet.petmily.review.dto;
 
 import com.mypet.petmily.member.dto.MemberDTO;
+import com.mypet.petmily.payment.dto.PetMemberDTO;
 import com.mypet.petmily.petSitter.dto.PetSitterDTO;
 import com.mypet.petmily.petSitterNew.dto.ReservationDTO;
 import lombok.Getter;
@@ -22,7 +23,7 @@ public class ReviewDTO {
     private String revStatus;               // 상태(Y=등록, N=삭제)
     private Date revDeleteDate;             // 삭제일
     private MemberDTO member;              // 회원
-    private PetSitterDTO petSitter;        // 펫시터
+    private PetMemberDTO petSitter;        // 펫시터
     private ReservationDTO reservation;     // 예약
     //private List<FileDTO> attachmentList; // 첨부파일(사진)
 }
